974c2ae821d6090c286b029e6307e547f8134ad5
Edilo <edilo13@gmail.com>
2020-06-25 16:32:26 -0400

    commit na busca de colaborador

--- class/colaborador.class.php
+++ class/colaborador.class.php
@@ -45,8 +45,53 @@ class Colaboradores
         echo $stmt->rowCount();
     }
 
-    function dadosGraficoColaborador(){
+    function dadosGraficoColaborador()
+    {
         $colaboradores = $this->conn->query("SELECT cargo,COUNT(*) as qtd FROM " . $this->table_name . " GROUP BY cargo ORDER BY cargo asc");
         return $colaboradores;
+    }
+
+    function buscarColaborador($valor)
+    {
+        define("URL","http://localhost/phpcrud/phpcrud/");
+        $colaboradores = $this->conn->query("SELECT * FROM colaboradores WHERE nome LIKE '%" . $valor . "%' OR telefone like '%" . $valor . "%' OR empresa like '%" . $valor . "%' 
+                        OR setor like '%" . $valor . "%' OR email like '%" . $valor . "%' OR cargo like '%" . $valor . "%'");
+?>
+        <table class="table table-hover table_colaborador" style="font-size: 14px;">
+            <thead>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>TELEFONE</th>
+                <th>EMPRESA</th>
+                <th>SETOR</th>
+                <th>CARGO</th>
+                <th>E-MAIL</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($colaboradores as $row) :
+                ?>
+                    <tr>
+                        <td>
+                            <span class="badge badge-danger">
+                                <?= $row['id'] ?>
+                            </span>
+                        </td>
+                        <td><?= $row['nome'] ?></td>
+                        <td><?= $row['telefone'] ?></td>
+                        <td><?= $row['empresa'] ?></td>
+                        <td><?= $row['setor'] ?></td>
+                        <td><?= $row['cargo'] ?></td>
+                        <td><?= $row['email'] ?></td>
+                        <td>
+                            <a class="btn btn-sm btn-info" href="<?= URL ?>views/colaboradores/detalhesColaborador.php?id=<?= $row['id'] ?>">Editar</a>
+                        </td>
+                    </tr>
+                <?php
+                endforeach;
+                ?>
+            </tbody>
+    <?php
     }
 }
--- php/buscarColaborador.php
+++ php/buscarColaborador.php
@@ -1,44 +1,12 @@
 <?php
-include '../config/conexao.php';
+include_once '../config/conexao.php';
+include_once '../class/colaborador.class.php';
+
 $valor = $_POST['valor'];
-$data = $conn->query("SELECT * FROM colaboradores WHERE nome LIKE '%" . $valor . "%' OR telefone like '%". $valor ."%' OR empresa like '%". $valor ."%' 
-                        OR setor like '%". $valor ."%' OR email like '%". $valor ."%' OR cargo like '%". $valor ."%'");
 
-?>
+$database = new Database();
+$db = $database->getConnection();
 
-<table class="table table-hover table_colaborador"  style="font-size: 14px;">
-    <thead>
-        <th>ID</th>
-        <th>NOME</th>
-        <th>TELEFONE</th>
-        <th>EMPRESA</th>
-        <th>SETOR</th>
-        <th>CARGO</th>
-        <th>E-MAIL</th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php
-        foreach ($data as $row) :
-        ?>
-            <tr>
-                <td>
-                    <span class="badge badge-danger">
-                        <?= $row['id'] ?>
-                    </span>
-                </td>
-                <td><?= $row['nome'] ?></td>
-                <td><?= $row['telefone'] ?></td>
-                <td><?= $row['empresa'] ?></td>
-                <td><?= $row['setor'] ?></td>
-                <td><?= $row['cargo'] ?></td>
-                <td><?= $row['email'] ?></td>
-                <td>
-                    <a class="btn btn-sm btn-info" href="<?= URL ?>views/colaboradores/detalhesColaborador.php?id=<?= $row['id'] ?>">Editar</a>
-                </td>
-            </tr>
-        <?php
-        endforeach;
-        ?>
-    </tbody>
-</table>
+$colaborador = new Colaboradores($db);
+$colaborador->buscarColaborador($valor);
+
